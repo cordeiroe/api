1c890afa5a49626b944593b9e8d3e09a3f72747e
cordeiroe <emersoncmarques@outlook.com>
2024-06-04 00:56:31 -0300

    .

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Alo, vc está na home';
-  }
-}
---
+++ src/users/users.service.ts
@@ -0,0 +1,65 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const isEmailAlreadyInUse = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (!isEmailAlreadyInUse) {
+      const hashPass = await hash(password, 12);
+
+      const user = await this.prismaService.user.create({
+        data: {
+          name,
+          email,
+          password: hashPass,
+          categories: {
+            createMany: {
+              data: [
+                // Income
+                { descCategory: 'Salário', icon: 'salary', type: 'INCOME' },
+                {
+                  descCategory: 'Freelance',
+                  icon: 'freelance',
+                  type: 'INCOME',
+                },
+                { descCategory: 'Outro', icon: 'other', type: 'INCOME' },
+                // Expense
+                { descCategory: 'Casa', icon: 'home', type: 'EXPENSE' },
+                { descCategory: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+                {
+                  descCategory: 'Educação',
+                  icon: 'education',
+                  type: 'EXPENSE',
+                },
+                { descCategory: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+                { descCategory: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+                { descCategory: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+                {
+                  descCategory: 'Transporte',
+                  icon: 'transport',
+                  type: 'EXPENSE',
+                },
+                { descCategory: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+                { descCategory: 'Outro', icon: 'other', type: 'EXPENSE' },
+              ],
+            },
+          },
+        },
+      });
+      return { message: 'Usuário criado!', name: user.name, email: user.email };
+    }
+
+    throw new ConflictException('Email já está em uso.');
+  }
+}
